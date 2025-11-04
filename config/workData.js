0f4eca130ecc9c2e9e3b20099eea701810e8e414
yashlakhotiya <lakhotiyayash@gmail.com>
2025-11-05 00:15:55 +0530

    fix structure

--- config/workData.js
+++ config/workData.js
@@ -13,7 +13,7 @@ const workData = [
             "#ISO 400\n" +
             "\n" +
             "I hope you will like my Timelapse video.\n",
-        image: "assets/images/night_startlapse.webp",
+        image: "assets/images/portfolio/night_startlapse.webp",
         link: "https://www.youtube.com/watch?v=BUAdIo5CKNo",
     },
     {
@@ -25,7 +25,7 @@ const workData = [
     {
         title: "Me, My device and The Universe",
         size: "tall-small",
-        image: "assets/images/stars_dslr.webp",
+        image: "assets/images/portfolio/stars_dslr.webp",
         link: "https://www.instagram.com/reel/C5jMjaqLgmq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
@@ -43,49 +43,49 @@ const workData = [
     {
         title: "Lunar Eclipse",
         size: "small",
-        image: "assets/images/lunar_eclipse.webp",
+        image: "assets/images/portfolio/lunar_eclipse.webp",
         link: "https://www.instagram.com/reel/DOks4boEgrN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Solar Eclipse",
         size: "tall-small",
-        image: "assets/images/solar_eclipse.webp",
+        image: "assets/images/portfolio/solar_eclipse.webp",
         link: "https://www.instagram.com/reel/C5jMjaqLgmq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Wildlife",
         size: "small",
-        image: "assets/images/wildlife.webp",
+        image: "assets/images/portfolio/wildlife.webp",
         link: "https://www.instagram.com/reel/Cv3tHXstDJJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Day to Night Timelapse",
         size: "small",
-        image: "assets/images/day_to_night_timelapse.webp",
+        image: "assets/images/portfolio/day_to_night_timelapse.webp",
         link: "https://www.instagram.com/reel/DMJ_m5xSMKG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Orion Nebula",
         size: "wide-small",
-        image: "assets/images/orion_nebula.webp",
+        image: "assets/images/portfolio/orion_nebula.webp",
         link: "https://www.instagram.com/p/DBkvJ2LPin-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Moon Alignment",
         size: "small",
-        image: "assets/images/moon_alignment.webp",
+        image: "assets/images/portfolio/moon_alignment.webp",
         link: "https://www.instagram.com/p/CmsbKFqvyV0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Comet C/2023 A3 (Tsuchinshan–ATLAS)",
         size: "tall-small",
-        image: "assets/images/comet.webp",
+        image: "assets/images/portfolio/comet.webp",
         link: "https://www.instagram.com/p/DApbUEEPiLd/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     },
     {
         title: "Andromeda Galaxy",
         size: "medium",
-        image: "assets/images/andromeda_galaxy.heic",
+        image: "assets/images/portfolio/andromeda_galaxy.heic",
         link: "https://www.instagram.com/p/DAswYEzPzaZ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
     }
 ];
