f91a41570ccdb6c71b77c2bc0e9d0c4915699dcf
yashlakhotiya <lakhotiyayash@gmail.com>
2025-11-05 01:01:02 +0530

    fix cdn url

--- config/env.js
+++ config/env.js
@@ -2,10 +2,13 @@
 const isProduction = typeof window !== 'undefined' &&
     (window.location.hostname === 'wiggleframes.com' ||
         window.location.hostname === 'www.wiggleframes.com' ||
-            window.location.hostname.endsWith('biodata-1ey.pages.dev'));
+        window.location.hostname.endsWith('biodata-1ey.pages.dev'));
+
+const protocol = window.location.protocol;
+const hostname = window.location.hostname;
 
 export default {
     IS_PRODUCTION: isProduction,
-    BASE_URL: isProduction ? window.location.hostname : 'http://localhost:8000',
-    CDN_URL: window.location.hostname
+    BASE_URL: isProduction ? `${protocol}//${hostname}` : 'http://localhost:8000',
+    CDN_URL: isProduction ? `${protocol}//${hostname}` : 'http://localhost:8000'
 };
