d187d249eac2f3dbdef512c8cc14e948852a3f8e
yashlakhotiya <lakhotiyayash@gmail.com>
2025-11-05 01:34:30 +0530

    fix

--- config/profilePictures.js
+++ config/profilePictures.js
@@ -1,18 +1,30 @@
 // Profile pictures array with carousel data - made globally available
 window.photoUrls = [
     {
-        url: 'assets/images/profile/photo1.png',
+        url: 'assets/images/profile/20240420_135806.webp',
     },
     {
-        url: 'assets/images/profile/photo2.jpg',
+        url: 'assets/images/profile/20240504_132348.webp',
     },
     {
-        url: 'assets/images/profile/photo3.jpg',
+        url: 'assets/images/profile/20241130_163336.webp',
     },
     {
-        url: 'assets/images/profile/photo4.jpg',
+        url: 'assets/images/profile/20250223_083325.webp',
     },
     {
-        url: 'assets/images/profile/photo5.jpg',
+        url: 'assets/images/profile/20250224_144414.webp',
+    },
+    {
+        url: 'assets/images/profile/photo1.webp',
+    },
+    {
+        url: 'assets/images/profile/photo3.webp',
+    },
+    {
+        url: 'assets/images/profile/photo4.webp',
+    },
+    {
+        url: 'assets/images/profile/photo5.webp',
     }
 ];
