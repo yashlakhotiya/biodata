caad2ae6d06e6a587d691dfeb7e31849659428ad
yashlakhotiya <lakhotiyayash@gmail.com>
2025-11-05 00:54:10 +0530

    update hostname

--- config/env.js
+++ config/env.js
@@ -1,7 +1,8 @@
 // config/env.js
 const isProduction = typeof window !== 'undefined' &&
     (window.location.hostname === 'wiggleframes.com' ||
-        window.location.hostname === 'www.wiggleframes.com');
+        window.location.hostname === 'www.wiggleframes.com' ||
+            window.location.hostname.endsWith('biodata-1ey.pages.dev'));
 
 export default {
     IS_PRODUCTION: isProduction,
